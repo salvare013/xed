ede7d2b077ee8497844ecf3805b19cf0d2ff0ed2
salvare013 <salvare00013@foxmail.com>
2025-09-24 00:18:38 +0800

    add icon

--- menubar.cpp
+++ menubar.cpp
@@ -7,73 +7,91 @@ MenuBar::MenuBar(QWidget *parent) : QMenuBar{parent} {
   menuView_ = addMenu(tr("视图(&V)"));
 
   // file action
-  actNewText_ = menuFile_->addAction(tr("新建文本文件(&N)"));
+  actNewText_ = menuFile_->addAction(QIcon(":/resfiles/icon/N.png"),
+                                     tr("新建文本文件(&N)"));
   actNewText_->setShortcut(Qt::CTRL + Qt::Key_N);
 
-  actNew_ = menuFile_->addAction(tr("新建文件...(&F)"));
+  actNew_ = menuFile_->addAction(QIcon(":/resfiles/icon/F.png"),
+                                 tr("新建文件...(&F)"));
   actNew_->setShortcut(Qt::CTRL + Qt::ALT + Qt::Key_N);
 
   menuFile_->addSeparator();
 
-  actOpen_ = menuFile_->addAction(tr("打开文件...(&O)"));
+  actOpen_ = menuFile_->addAction(QIcon(":/resfiles/icon/O.png"),
+                                  tr("打开文件...(&O)"));
   actOpen_->setShortcut(Qt::CTRL + Qt::Key_O);
 
   menuFile_->addSeparator();
 
-  actSave_ = menuFile_->addAction(tr("保存文件(&S)"));
+  actSave_ = menuFile_->addAction(QIcon(":/resfiles/icon/S.png"),
+                                  tr("保存文件(&S)"));
   actSave_->setShortcut(Qt::CTRL + Qt::Key_S);
 
-  actSaveAs_ = menuFile_->addAction(tr("另存为文件...(&A)"));
+  actSaveAs_ = menuFile_->addAction(QIcon(":/resfiles/icon/A.png"),
+                                    tr("另存为文件...(&A)"));
   actSaveAs_->setShortcut(Qt::CTRL + Qt::ALT + Qt::Key_S);
 
-  actSaveAll_ = menuFile_->addAction(tr("保存所有文件(&L)"));
+  actSaveAll_ = menuFile_->addAction(QIcon(":/resfiles/icon/L.png"),
+                                     tr("保存所有文件(&L)"));
   actSaveAll_->setShortcut(Qt::CTRL + Qt::SHIFT + Qt::Key_S);
 
   menuFile_->addSeparator();
 
-  actClose_ = menuFile_->addAction(tr("关闭文件(&W)"));
+  actClose_ = menuFile_->addAction(QIcon(":/resfiles/icon/W.png"),
+                                   tr("关闭文件(&W)"));
   actClose_->setShortcut(Qt::CTRL + Qt::Key_W);
 
-  actCloseAll_ = menuFile_->addAction(tr("关闭所有文件(&X)"));
+  actCloseAll_ = menuFile_->addAction(QIcon(":/resfiles/icon/X.png"),
+                                      tr("关闭所有文件(&X)"));
   actCloseAll_->setShortcut(Qt::CTRL + Qt::SHIFT + Qt::Key_W);
 
   menuFile_->addSeparator();
 
-  actQuit_ = menuFile_->addAction(tr("退出程序(&Q)"));
+  actQuit_ = menuFile_->addAction(QIcon(":/resfiles/icon/Q.png"),
+                                  tr("退出程序(&Q)"));
   actQuit_->setShortcut(Qt::CTRL + Qt::Key_Q);
 
   //   edit action
-  actUndo_ = menuEdit_->addAction(tr("撤销(&U)"));
+  actUndo_ = menuEdit_->addAction(QIcon(":/resfiles/icon/U.png"),
+                                  tr("撤销(&U)"));
   actUndo_->setShortcut(Qt::CTRL + Qt::Key_Z);
 
-  actRedo_ = menuEdit_->addAction(tr("恢复(&R)"));
+  actRedo_ = menuEdit_->addAction(QIcon(":/resfiles/icon/R.png"),
+                                  tr("恢复(&R)"));
   actRedo_->setShortcut(Qt::CTRL + Qt::Key_Y);
 
   menuEdit_->addSeparator();
 
-  actCut_ = menuEdit_->addAction(tr("剪切(&T)"));
+  actCut_ = menuEdit_->addAction(QIcon(":/resfiles/icon/T.png"),
+                                 tr("剪切(&T)"));
   actCut_->setShortcut(Qt::CTRL + Qt::Key_X);
 
-  actCopy_ = menuEdit_->addAction(tr("复制(&C)"));
+  actCopy_ = menuEdit_->addAction(QIcon(":/resfiles/icon/C.png"),
+                                  tr("复制(&C)"));
   actCopy_->setShortcut(Qt::CTRL + Qt::Key_C);
 
-  actPaste_ = menuEdit_->addAction(tr("粘贴(&V)"));
+  actPaste_ = menuEdit_->addAction(QIcon(":/resfiles/icon/P.png"),
+                                   tr("粘贴(&P)"));
   actPaste_->setShortcut(Qt::CTRL + Qt::Key_V);
 
   menuEdit_->addSeparator();
 
-  actSelectAll_ = menuEdit_->addAction(tr("全选(&A)"));
+  actSelectAll_ = menuEdit_->addAction(
+      QIcon(":/resfiles/icon/A.png"), tr("全选(&A)"));
   actSelectAll_->setShortcut(Qt::CTRL + Qt::Key_A);
 
   menuEdit_->addSeparator();
 
-  actGo_ = menuEdit_->addAction(tr("转到(&G)"));
+  actGo_ = menuEdit_->addAction(QIcon(":/resfiles/icon/G.png"),
+                                tr("转到(&G)"));
   actGo_->setShortcut(Qt::CTRL + Qt::Key_G);
   // view action
-  actZoomInFont_ = menuView_->addAction(tr("放大编辑字体(&P)"));
+  actZoomInFont_ = menuView_->addAction(
+      QIcon(":/resfiles/icon/P.png"), tr("放大编辑字体(&P)"));
   actZoomInFont_->setShortcut(Qt::CTRL + Qt::Key_Equal);
 
-  actZoomOutFont_ = menuView_->addAction(tr("缩小编辑字体(&M)"));
+  actZoomOutFont_ = menuView_->addAction(
+      QIcon(":/resfiles/icon/M.png"), tr("缩小编辑字体(&M)"));
   actZoomOutFont_->setShortcut(Qt::CTRL + Qt::Key_Minus);
 
   menuView_->addSeparator();
