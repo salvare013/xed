89722d753e5fb56ceb348155af6f69e0cd868024
salvare013 <salvare00013@foxmail.com>
2025-09-28 20:29:36 +0800

    remove vcpkg.json add codecs

--- statubar.cpp
+++ statubar.cpp
@@ -1,8 +1,11 @@
 #include "statubar.h"
+#include <QTextCodec>
 
 StatuBar::StatuBar(QWidget *parent) : QStatusBar{parent} {
   codecBox_ = new QComboBox(this);
-  codecBox_->addItems({"UTF-8", "ANSI", "GBK", "GB18030", "UTF-16"});
+  for (auto e : QTextCodec::availableCodecs()) {
+    codecBox_->addItem(e);
+  }
 
   fontLabel_ = new QLabel(this);
   PosLabel_ = new QLabel(this);
